a69d6a96b8189e8ec6e73ebb0ee001d50ded74aa
cbrow353 <cbrow353@uncc.edu>
2024-05-29 19:08:43 -0400

    setup layouts

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g
+import requests
 
 app = Flask(__name__)
 
 # database connection
 
 # variables
+app.secret_key = "secret key"
 
 # AWS S3 connection (for member photos)
 
-
+# check if the user is logged in
+@app.before_request
+def before_request():
+    g.user = None
+    if 'username' in session:
+        g.user = session['username']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/contact')
 def contact():
@@ -25,6 +32,48 @@ def gallary():
 @app.route('/member')
 def member():
     return render_template('member.html')
+
+@app.route('/admin')
+def admin():
+    if 'username' in session:
+        return render_template('admin.html')
+    return redirect(url_for('login'))
+
+# login to the admin page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get the username and password
+        username = request.form['username']
+        password = request.form['password']
+        # check if the username and password are correct
+        if username == 'admin' and password == 'admin':
+            # if correct, redirect to the admin page
+            session['username'] = username
+            return redirect(url_for('admin'))
+        else:
+            # if incorrect, show an error message
+            flash('Invalid username or password', 'error')
+    return render_template('login.html')
+
+# logout
+@app.route('/logout')
+def logout():
+    # remove the username from the session
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# add member
+@app.route('/addmember', methods=['POST'])
+def addmember():
+    if 'username' in session:
+        # get the member details
+        email = request.form['email']
+        # send email to the email address [todo] ************
+
+        # redirect to the admin page
+        return redirect(url_for('admin'))
+    return redirect(url_for('login'))
 
 # error page
 @app.errorhandler(404)
