ccdca7b0a54c0b5d32bbfdc17e778dcfe9557200
cbrow353 <cbrow353@uncc.edu>
2024-05-29 18:17:27 -0400

    setup pages and basic app.py

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-import flask
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+# database connection
+
+# variables
+
+# AWS S3 connection (for member photos)
+
+
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/gallary')
+def gallary():
+    return render_template('gallary.html')
+
+@app.route('/member')
+def member():
+    return render_template('member.html')
+
+# error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
