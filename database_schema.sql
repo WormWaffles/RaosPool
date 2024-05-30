a75a45644a18e2e8a66614ad90251b25917de7f1
cbrow353 <cbrow353@uncc.edu>
2024-05-30 07:53:27 -0400

    updated .gitignore and requirements

---
+++ database_schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE raospool;
+
+-- DROP TABLES IF THEY EXIST TO PREVENT ERRORS
+DROP TABLE IF EXISTS "user" CASCADE;
+
+-- WAIT TO ADD TABLES UNTIL AFTER THE DATABASE IS CREATED
+CREATE TABLE "user" (
+    user_id INTEGER NOT NULL,
+    first_name VARCHAR(80),
+    last_name VARCHAR(80),
+    email VARCHAR(80) NOT NULL,
+    password VARCHAR(80),
+    PRIMARY KEY (user_id),
+    -- picture paths (aws)
+    profile_image_location VARCHAR(255),
+
+    -- admin stuff
+    admin BOOLEAN NOT NULL,
+);
